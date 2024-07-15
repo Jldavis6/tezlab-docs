1c002181485138ed66f18eaf9a2c8ae50855de2a
Jack Davis <90731690+Jldavis6@users.noreply.github.com>
2024-07-15 12:49:39 -0400

    Add language files

---
+++ translations/index.js
@@ -0,0 +1,7 @@
+export { default as en } from "./en.json";
+export { default as es } from "./es.json";
+export { default as fr } from "./fr.json";
+export { default as de } from "./de.json";
+export { default as nb } from "./nb.json";
+export { default as it } from "./it.json";
+export { default as pt } from "./pt.json";
